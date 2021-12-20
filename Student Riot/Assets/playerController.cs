c96c2dce079086c2cab6c5ba0023c45e3848ded9
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2021-12-20 17:49:41 +0300

    + Feat : create playercontroller script

---
+++ Student Riot/Assets/playerController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Transform visual;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
